03e3066744a708cc1016a9ae5b0ae5e7440789eb
Alexey Tourbin <at@altlinux.ru>
2009-02-18 10:54:21 +0300

    bad_elf_symbols.pl: optimize inner loop for speed

--- bad_elf_symbols.pl
+++ bad_elf_symbols.pl
@@ -12,24 +12,15 @@ sub collect {
 	my $out = rpmelfsym $rpm;
 	use File::Basename 'basename';
 	my $rpm_bn = basename $rpm;
-	my @def =
-		map  { $$_[1] }
-		grep { $$_[0] =~ /^[A-TV-Z]/ }
-		map  { @{$$_[1]} } @$out;
-	my @ref =
-		grep { @{$$_[1]} }
-		map  { [ $$_[0], [
-				map { $$_[1] }
-				grep { $$_[0] eq "U" }
-				@{$$_[1]} ]
-		] } @$out;
-	for (@def) {
-		print $DEF $_, "\n";
-	}
-	for (@ref) {
-		my $prefix = "$rpm_bn\t$$_[0]";
-		for my $sym (@{$$_[1]}) {
-			print $REF $prefix, "\tU\t", $sym, "\n";
+	for my $file2syms (@$out) {
+		my $U_prefix = "$rpm_bn\t$$file2syms[0]\tU\t";
+		for my $sym (@{$$file2syms[1]}) {
+			if ($$sym[0] eq "U") {
+				print $REF $U_prefix, $$sym[1], "\n";
+			}
+			elsif ($$sym[0] =~ /^[A-TV-Z]/) {
+				print $DEF $$sym[1], "\n";
+			}
 		}
 	}
 }

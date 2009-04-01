49ad5443f5faa47f4f9d458487f7fb2249cc810e
Alexey Tourbin <at@altlinux.ru>
2009-04-01 12:43:43 +0400

    bad_elf_symbols*.pl: optimize by running sort(1) in background

--- bad_elf_symbols.pl
+++ bad_elf_symbols.pl
@@ -2,12 +2,8 @@
 
 use strict;
 
-require File::Temp;
-our $DEF = File::Temp->new;
-our $REF = File::Temp->new;
-
-sub collect {
-	my $rpm = shift;
+sub collect ($$$) {
+	my ($rpm, $def, $ref) = @_;
 	use qa::rpmelfsym 'rpmelfsym';
 	my $out = rpmelfsym $rpm;
 	use File::Basename 'basename';
@@ -16,14 +12,25 @@ sub collect {
 		my $U_prefix = "$rpm_bn\t$$file2syms[0]\tU\t";
 		for my $sym (@{$$file2syms[1]}) {
 			if ($$sym[0] eq "U") {
-				print $REF $U_prefix, $$sym[1], "\n";
+				print $ref $U_prefix, $$sym[1], "\n"
+					or die "sort failed";
 			}
 			elsif ($$sym[0] =~ /^[A-TV-Z]/) {
-				print $DEF $$sym[1], "\n";
+				print $def $$sym[1], "\n"
+					or die "sort failed";
 			}
 		}
 	}
 }
+
+use File::Temp 'tempdir';
+use sigtrap qw(die normal-signals);
+my $TMPDIR = $ENV{TMPDIR} = tempdir(CLEANUP => 1);
+
+open my $def, "|-", "sort", "-o", "$TMPDIR/def", "-u"
+	or die "sort failed";
+open my $ref, "|-", "sort", "-o", "$TMPDIR/ref", "-t\t", "-k4,4"
+	or die "sort failed";
 
 for my $arg (@ARGV) {
 	my @rpms;
@@ -34,14 +41,13 @@ for my $arg (@ARGV) {
 		@rpms = $arg;
 	}
 	for my $rpm (@rpms) {
-		collect $rpm;
+		collect $rpm, $def, $ref;
 	}
 }
 
-@ENV{qw(DEF REF)}=($DEF,$REF);
-system(<<'EOF') == 0 or die "/bin/sh failed";
-set -efu
-sort -u -o "$DEF" "$DEF"
-sort -t$'\t' -k4,4 -o "$REF" "$REF"
-join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' "$REF" "$DEF"
-EOF
+close $def or die "sort failed";
+close $ref or die "sort failed";
+
+0 == system "join", "-t\t", qw(-v1 -14 -21 -o), '1.1 1.2 1.3 1.4',
+	"$TMPDIR/ref", "$TMPDIR/def"
+		or die "join failed";
--- bad_elf_symbols_dircmp.pl
+++ bad_elf_symbols_dircmp.pl
@@ -34,7 +34,7 @@ my @rpms2;
 	@rpms2 = sort map { $$_[0] } values %rpms2;
 }
 
-sub collect {
+sub collect ($$$) {
 	my ($rpm, $def, $ref) = @_;
 	use qa::rpmelfsym 'rpmelfsym';
 	my $out = rpmelfsym $rpm;
@@ -44,64 +44,68 @@ sub collect {
 		my $U_prefix = "$rpm_bn\t$$file2syms[0]\tU\t";
 		for my $sym (@{$$file2syms[1]}) {
 			if ($$sym[0] eq "U") {
-				print $ref $U_prefix, $$sym[1], "\n";
+				print $ref $U_prefix, $$sym[1], "\n"
+					or die "sort failed";
 			}
 			elsif ($$sym[0] =~ /^[A-TV-Z]/) {
-				print $def $$sym[1], "\n";
+				print $def $$sym[1], "\n"
+					or die "sort failed";
 			}
 		}
 	}
 }
 
-require File::Temp;
+use File::Temp 'tempdir';
 use sigtrap qw(die normal-signals);
+my $TMPDIR = $ENV{TMPDIR} = tempdir(CLEANUP => 1);
 
-my $def1 = File::Temp->new;
-my $ref1 = File::Temp->new;
+open my $def1, "|-", "sort", "-o", "$TMPDIR/def1", "-u"
+	or die "sort failed";
+open my $ref1, "|-", "sort", "-o", "$TMPDIR/ref1", "-t\t", "-k4,4"
+	or die "sort failed";
 collect($_, $def1, $ref1) for @rpms1;
+close $def1 or die "sort failed";
+close $ref1 or die "sort failed";
 
-my $def2 = File::Temp->new;
-my $ref2 = File::Temp->new;
+open my $def2, "|-", "sort", "-o", "$TMPDIR/def2", "-u"
+	or die "sort failed";
+open my $ref2, "|-", "sort", "-o", "$TMPDIR/ref2", "-t\t", "-k4,4"
+	or die "sort failed";
 collect($_, $def2, $ref2) for @rpms2;
+close $def2 or die "sort failed";
+close $ref2 or die "sort failed";
 
-($def1->sync, -s $def1) or
-($ref1->sync, -s $ref1) or
-($def2->sync, -s $def2) or
-($ref2->sync, -s $ref2) or
+-s "$TMPDIR/def1" or
+-s "$TMPDIR/ref1" or
+-s "$TMPDIR/def2" or
+-s "$TMPDIR/ref2" or
 exit 0;
 
-my $def0 = File::Temp->new;
-my $ref0 = File::Temp->new;
+open my $def0, "|-", "sort", "-o", "$TMPDIR/def0", "-u"
+	or die "sort failed";
+open my $ref0, "|-", "sort", "-o", "$TMPDIR/ref0", "-t\t", "-k4,4"
+	or die "sort failed";
 collect($_, $def0, $ref0) for @rpms0;
+close $def0 or die "sort failed";
+close $ref0 or die "sort failed";
 
-my $tmp = File::Temp->new;
-
-@ENV{qw( tmp  def0  def1  def2  ref0  ref1  ref2)} =
-       ($tmp,$def0,$def1,$def2,$ref0,$ref1,$ref2);
-
-system(<<'EOF') == 0 or die "/bin/sh failed";
+0 == system <<'EOF' or die "/bin/sh failed";
 set -efu
-sort -u -o "$def0" "$def0"
-sort -t$'\t' -k4,4 -o "$ref0" "$ref0"
-join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' "$ref0" "$def0" >"$tmp"
-cat "$tmp" >"$ref0"
-: >"$tmp"
-sort -u -o "$def1" "$def1"
-sort -u -o "$def2" "$def2"
-sort -m -u -o "$def1" "$def1" "$def0"
-sort -m -u -o "$def2" "$def2" "$def0"
-: >"$def0"
-sort -t$'\t' -k4,4 -o "$ref1" "$ref1"
-sort -t$'\t' -k4,4 -o "$ref2" "$ref2"
-sort -m -t$'\t' -k4,4 -o "$ref1" "$ref1" "$ref0"
-sort -m -t$'\t' -k4,4 -o "$ref2" "$ref2" "$ref0"
-: >"$ref0"
-join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' "$ref1" "$def1" >"$tmp"
-sort -u -o "$ref1" "$tmp"
-: >"$def1"
-join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' "$ref2" "$def2" >"$tmp"
-sort -u -o "$ref2" "$tmp"
-: >"$def2"
-comm -13 "$ref1" "$ref2"
-comm -23 "$ref1" "$ref2" >&3
+cd "$TMPDIR"
+join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' ref0 def0 >tmp
+mv -f tmp ref0
+sort -m -u -o def1 def1 def0
+sort -m -u -o def2 def2 def0
+rm -f def0
+sort -m -t$'\t' -k4,4 -o ref1 ref1 ref0
+sort -m -t$'\t' -k4,4 -o ref2 ref2 ref0
+rm -f ref0
+join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' ref1 def1 >tmp
+sort -u -o ref1 tmp
+rm -f def1 tmp
+join -t$'\t' -v1 -14 -21 -o '1.1 1.2 1.3 1.4' ref2 def2 >tmp
+sort -u -o ref2 tmp
+rm -f def2 tmp
+comm -13 ref1 ref2
+comm -23 ref1 ref2 >&3
 EOF
